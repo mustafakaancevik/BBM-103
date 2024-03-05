3e7227f378302aa58aac5d359bde964743cd73be
mustafakaancevik <162364215+mustafakaancevik@users.noreply.github.com>
2024-03-05 23:14:05 +0300

    Delete Assignment 2 directory

--- Assignment 2/src/ERROR.java
+++
@@ -1,9 +0,0 @@
-public class ERROR extends Exception{
-    /**
-     * This meethod throws an exception with desired error messages.
-     * @param value String error message.
-     */
-    public ERROR(String value){
-        super(value);
-    }
-}
